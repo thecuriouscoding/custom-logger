a8aecf013397a329c90b1d780df681f58b5fb40e
Eshan Gupta <connectwithcuriouscoding@gmail.com>
2024-10-17 04:17:12 +0000

    corrected info mode

--- nodejs/logger.js
+++ nodejs/logger.js
@@ -17,7 +17,7 @@ const logger = (logType) => {
   };
 
   return {
-    log: (message) => log("log", message),
+    info: (message) => log("info", message),
     error: (message) => log("error", message),
     warn: (message) => log("warn", message),
     debug: (message) => log("debug", message),
